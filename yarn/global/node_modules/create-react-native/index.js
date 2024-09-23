88c873518142f2550656b308ef65c2977555dba3
Phil George <23001002+pg328@users.noreply.github.com>
2024-09-23 16:23:36 +0100

    Initial

---
+++ yarn/global/node_modules/create-react-native/index.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+console.log("\nOoops, you might have downloaded the wrong repository...\n");
+console.log("\nYou might be looking for the create-react-native application\n");
+console.log("\nMore information could be found here: https://github.com/react-community/create-react-native-app/blob/master/README.md\n");
