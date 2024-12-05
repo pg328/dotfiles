193a05ac2c7c945ac2a65189fdbac0d8c02305a1
Phil George <23001002+pg328@users.noreply.github.com>
2024-12-05 18:19:13 +0000

    package: p7zip

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -50,6 +50,7 @@
     wofi
     duplicati
     tmux
+    p7zip
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
